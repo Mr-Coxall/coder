528f22b8159a186a4581f4b6ff7443105b401614
Patrick Coxall <mr.coxall@mths.ca>
2024-10-16 12:38:42 +0000

    update

--- site/src/theme/globalFonts.ts
+++ site/src/theme/globalFonts.ts
@@ -1,5 +1,6 @@
 // Monospace fonts used for code, button styles, and banners
 // updated to new font, to include nerd fonts
+
 import "@fontsource/fira-code/400.css";
 import "@fontsource/fira-code/600.css";
 // Main body copy font
