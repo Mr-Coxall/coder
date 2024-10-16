7c1586c221b4def1eefc4993cb1e631124e916d4
Patrick Coxall <mr.coxall@mths.ca>
2024-10-16 12:37:56 +0000

    change web terminal font

--- site/src/theme/globalFonts.ts
+++ site/src/theme/globalFonts.ts
@@ -1,5 +1,6 @@
 // Monospace fonts used for code, button styles, and banners
-import "@fontsource/ibm-plex-mono/400.css";
-import "@fontsource/ibm-plex-mono/600.css";
+// updated to new font, to include nerd fonts
+import "@fontsource/fira-code/400.css";
+import "@fontsource/fira-code/600.css";
 // Main body copy font
 import "@fontsource-variable/inter";
